3121bc8d90f1291a06de8fdca1d1eae299688b10
Joy10311 <joy10311@gmail.com>
2021-10-04 22:58:08 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, } from 'react-router-dom';
 import './App.css';
 import About from './components/About/About';
 import Class from './components/Class/Class';
@@ -6,7 +6,6 @@ import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import NotFound from './components/NotFound/NotFound';
-import SetClass from './components/SetClass/SetClass';
 import Trainers from './components/Trainers/Trainers';
 
 function App() {
@@ -15,10 +14,12 @@ function App() {
       <Router>
         <Header></Header>
         <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
           <Route exact path="/home">
             <Home></Home>
           </Route>
-
           <Route exact path="/class">
             <Class></Class>
           </Route>
@@ -28,13 +29,11 @@ function App() {
           <Route exact path="/about">
             <About></About>
           </Route>
-          <Route path="/*">
+          <Route path="*">
             <NotFound></NotFound>
           </Route>
-          <Route exact path="/">
-            <Home></Home>
-          </Route>
         </Switch>
+        <Footer></Footer>
       </Router>
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../image/logo.png'
-import NotFound from '../NotFound/NotFound';
+
 
 
 const Header = () => {
